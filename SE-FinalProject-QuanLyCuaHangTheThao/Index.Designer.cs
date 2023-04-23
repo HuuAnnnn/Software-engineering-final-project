7b2a2148f3653b6a57321d063652c10284fe1219
NguyenHuuAn <huuan04112002@gmail.com>
2023-04-24 04:39:13 +0700

    fix merge confic

--- SE-FinalProject-QuanLyCuaHangTheThao/Index.Designer.cs
+++ SE-FinalProject-QuanLyCuaHangTheThao/Index.Designer.cs
@@ -28,10 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.totalTurnOver = new System.Windows.Forms.Label();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.displayEmplTitle = new System.Windows.Forms.Label();
             this.displayEmpUsername = new System.Windows.Forms.Label();
@@ -40,69 +36,12 @@
             this.lblUsername = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.imgUsername = new System.Windows.Forms.PictureBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.totalCustomers = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.totalProducts = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgUsername)).BeginInit();
-            this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.panel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(4)))), ((int)(((byte)(41)))));
-            this.panel2.Controls.Add(this.totalTurnOver);
-            this.panel2.Controls.Add(this.pictureBox4);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(446, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(438, 202);
-            this.panel2.TabIndex = 1;
-            // 
-            // totalTurnOver
-            // 
-            this.totalTurnOver.AutoSize = true;
-            this.totalTurnOver.Font = new System.Drawing.Font("Quicksand", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.totalTurnOver.ForeColor = System.Drawing.Color.White;
-            this.totalTurnOver.Location = new System.Drawing.Point(75, 89);
-            this.totalTurnOver.Name = "totalTurnOver";
-            this.totalTurnOver.Size = new System.Drawing.Size(363, 55);
-            this.totalTurnOver.TabIndex = 3;
-            this.totalTurnOver.Text = "1,000,000,000 VNĐ";
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = global::SE_FinalProject_QuanLyCuaHangTheThao.Properties.Resources.money_receive;
-            this.pictureBox4.Location = new System.Drawing.Point(7, 87);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(64, 64);
-            this.pictureBox4.TabIndex = 2;
-            this.pictureBox4.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Quicksand", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(438, 40);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Tổng doanh thu trong ngày";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel1
             // 
@@ -113,6 +52,8 @@
             this.panel1.Controls.Add(this.lblUsername);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.imgUsername);
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
@@ -153,6 +94,16 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(437, 202);
 >>>>>>> Stashed changes
+=======
+            this.panel1.Location = new System.Drawing.Point(241, 141);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(437, 195);
+>>>>>>> Stashed changes
+=======
+            this.panel1.Location = new System.Drawing.Point(241, 141);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(437, 195);
+>>>>>>> Stashed changes
             this.panel1.TabIndex = 0;
             // 
             // displayEmplTitle
@@ -185,6 +136,8 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< HEAD
             this.lblWelcome.Location = new System.Drawing.Point(26, 14);
 =======
@@ -204,6 +157,12 @@
 >>>>>>> Stashed changes
 =======
             this.lblWelcome.Location = new System.Drawing.Point(26, 18);
+>>>>>>> Stashed changes
+=======
+            this.lblWelcome.Location = new System.Drawing.Point(26, 14);
+>>>>>>> Stashed changes
+=======
+            this.lblWelcome.Location = new System.Drawing.Point(26, 14);
 >>>>>>> Stashed changes
             this.lblWelcome.Name = "lblWelcome";
             this.lblWelcome.Size = new System.Drawing.Size(146, 43);
@@ -239,6 +198,8 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< HEAD
             this.pictureBox1.Location = new System.Drawing.Point(34, 134);
 =======
@@ -259,6 +220,12 @@
 =======
             this.pictureBox1.Location = new System.Drawing.Point(34, 138);
 >>>>>>> Stashed changes
+=======
+            this.pictureBox1.Location = new System.Drawing.Point(34, 134);
+>>>>>>> Stashed changes
+=======
+            this.pictureBox1.Location = new System.Drawing.Point(34, 134);
+>>>>>>> Stashed changes
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(32, 32);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -269,6 +236,8 @@
             // 
             this.imgUsername.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.imgUsername.Image = global::SE_FinalProject_QuanLyCuaHangTheThao.Properties.Resources.username_index_1;
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
@@ -294,100 +263,22 @@
 =======
             this.imgUsername.Location = new System.Drawing.Point(34, 82);
 >>>>>>> Stashed changes
+=======
+            this.imgUsername.Location = new System.Drawing.Point(34, 78);
+>>>>>>> Stashed changes
+=======
+            this.imgUsername.Location = new System.Drawing.Point(34, 78);
+>>>>>>> Stashed changes
             this.imgUsername.Name = "imgUsername";
             this.imgUsername.Size = new System.Drawing.Size(32, 32);
             this.imgUsername.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imgUsername.TabIndex = 9;
             this.imgUsername.TabStop = false;
             // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(134)))), ((int)(((byte)(255)))));
-            this.panel3.Controls.Add(this.totalCustomers);
-            this.panel3.Controls.Add(this.pictureBox2);
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(446, 223);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(438, 198);
-            this.panel3.TabIndex = 4;
-            // 
-            // totalCustomers
-            // 
-            this.totalCustomers.AutoSize = true;
-            this.totalCustomers.Font = new System.Drawing.Font("Quicksand", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.totalCustomers.ForeColor = System.Drawing.Color.White;
-            this.totalCustomers.Location = new System.Drawing.Point(139, 99);
-            this.totalCustomers.Name = "totalCustomers";
-            this.totalCustomers.Size = new System.Drawing.Size(224, 55);
-            this.totalCustomers.TabIndex = 5;
-            this.totalCustomers.Text = "1000 khách";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::SE_FinalProject_QuanLyCuaHangTheThao.Properties.Resources.customers;
-            this.pictureBox2.Location = new System.Drawing.Point(52, 96);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
-            // 
-            // label2
-            // 
-            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label2.Font = new System.Drawing.Font("Quicksand", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(0, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(438, 52);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Số lượt khách hàng trong ngày";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(214)))), ((int)(((byte)(160)))));
-            this.panel4.Controls.Add(this.totalProducts);
-            this.panel4.Controls.Add(this.pictureBox3);
-            this.panel4.Controls.Add(this.label4);
-            this.panel4.Location = new System.Drawing.Point(3, 223);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(437, 198);
-            this.panel4.TabIndex = 5;
-            // 
-            // totalProducts
-            // 
-            this.totalProducts.AutoSize = true;
-            this.totalProducts.Font = new System.Drawing.Font("Quicksand", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.totalProducts.ForeColor = System.Drawing.Color.White;
-            this.totalProducts.Location = new System.Drawing.Point(110, 104);
-            this.totalProducts.Name = "totalProducts";
-            this.totalProducts.Size = new System.Drawing.Size(292, 55);
-            this.totalProducts.TabIndex = 4;
-            this.totalProducts.Text = "1000 sản phẩm";
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::SE_FinalProject_QuanLyCuaHangTheThao.Properties.Resources.products;
-            this.pictureBox3.Location = new System.Drawing.Point(40, 99);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(64, 64);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
-            // 
-            // label4
-            // 
-            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label4.Font = new System.Drawing.Font("Quicksand", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(0, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(437, 84);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Số lượng sản phẩm đã bán trong ngày";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // tableLayoutPanel1
             // 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
@@ -432,12 +323,28 @@
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
+=======
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+=======
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+>>>>>>> Stashed changes
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 443F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 230F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.93458F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49.06542F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 201F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 194F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
@@ -453,9 +360,12 @@
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 58);
 =======
 >>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.93458F));
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 278F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 83F));
@@ -481,6 +391,16 @@
 =======
             this.tableLayoutPanel1.Size = new System.Drawing.Size(887, 432);
 >>>>>>> Stashed changes
+=======
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(911, 533);
+>>>>>>> Stashed changes
+=======
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 201F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 194F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(911, 533);
+>>>>>>> Stashed changes
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // Index
@@ -496,30 +416,16 @@
             this.Name = "Index";
             this.Text = " ";
             this.Load += new System.EventHandler(this.Index_Load);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgUsername)).EndInit();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label totalTurnOver;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label displayEmplTitle;
         private System.Windows.Forms.Label displayEmpUsername;
@@ -528,14 +434,6 @@
         private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox imgUsername;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Label totalCustomers;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Label totalProducts;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
